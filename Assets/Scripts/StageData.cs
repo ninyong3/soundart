430dd46d74864288d77af8ed527469a59c003e81
ninyong3 <yongpoke06@gmail.com>
2026-02-27 00:46:16 +0900

    스테이지 선택 위치만 잡음

---
+++ Assets/Scripts/StageData.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Stage", menuName = "RhythmGame/StageData")]
+public class StageData: ScriptableObject
+{
+    [Header("기본 정보")]
+    public string songTitle;
+    public string composer;
+    [Header("리소스")]
+    public Sprite coverImage;
+    public AudioClip musicClip; // 미리듣기용 배경음악
+    [Header("게임 설정")]
+    public string gameSceneName;
+    public float bpm;
+    public int difficultyLevel;
+}
